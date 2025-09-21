fbf9f4405c7c79f7ca9cb115b0ff4609c517e247
Stan Syfert <stanguldemond@gmail.com>
2025-09-21 18:23:32 +0200

    Tunnel to Portal via Cloudflare

--- projects/portal/my-portal/vite.config.ts
+++ projects/portal/my-portal/vite.config.ts
@@ -4,9 +4,9 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
-		allowedHosts: ['portal.macmini.home']
+		allowedHosts: ['portal.macmini.home', 'homelab.stansyfert.com']
 	},
 	preview: {
-		allowedHosts: ['portal.macmini.home']
+		allowedHosts: ['portal.macmini.home', 'homelab.stansyfert.com']
 	}
 });
